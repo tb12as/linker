5a427e93f9f3457f2df2a515c7e47bb326ecd972
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2023-06-01 23:47:20 +0700

    fix: route order

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('{any}', 'app')->where('any', '.*');
 
 Route::get('r/{code}', function ($uniqueCode) {
     $link = Link::where('unique_code', $uniqueCode)->first();
@@ -29,6 +28,8 @@ Route::get('r/{code}', function ($uniqueCode) {
 
     abort(404);
 })->name('red');
+
+Route::view('{any}', 'app')->where('any', '.*');
 
 Route::get('/', function () {
     return view('welcome');
