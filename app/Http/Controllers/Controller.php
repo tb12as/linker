2adc94fc0ab847a03797478a0839755be5844f29
Syafiq Afifuddin <syafiqafifuddin59@gmail.com>
2024-11-10 16:23:17 +0700

    fix: api response function in base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,18 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function ok($data, $code = 200)
+    {
+        if (is_string($data)) {
+            $data = ['message' => $data];
+        }
+
+        return response()->json($data, $code);
+    }
+
+    public function fail($data, $code = 403)
+    {
+        return $this->ok($data, $code);
+    }
 }
